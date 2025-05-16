70548beb433b6802fb6e1278be0df3999c848226
edwo-rei <edwo-rei@student.42malaga.com>
2025-05-16 19:44:07 +0200

    Updated ft_printf/

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: edwo-rei <edwo-rei@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 17:03:22 by edwo-rei          #+#    #+#             */
-/*   Updated: 2025/05/16 18:11:43 by edwo-rei         ###   ########.fr       */
+/*   Updated: 2025/05/16 19:43:36 by edwo-rei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,20 @@
 
 int	ft_printf(const char *s, ...)
 {
+	int	i;
+	int	count;//will need to count # of args somehow
 	va_list	args;
+
+	//initialize va_list
+	va_start(args, s);
+	count = va_arg(args, /*type of variable*/);
+	i = 0;
+	while(count != NULL)
+	{
+		count = va_arg(args, /*type of var*/);
+	}
+	//call va_end once done processing list
+	va_end(args);
 }
 
 int	main(void)
--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: edwo-rei <edwo-rei@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:59:45 by edwo-rei          #+#    #+#             */
-/*   Updated: 2025/05/16 17:44:37 by edwo-rei         ###   ########.fr       */
+/*   Updated: 2025/05/16 19:40:30 by edwo-rei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@
 # define LIBFTPRINTF_H
 //stdarg library includes variable that allows for indefinite # of args
 # include <stdarg.h>
+//stdlib contains malloc & free
 # include <stdlib.h>
+//unistd library contains write
+# include <unistd.h>
+//stdio library contains the established printf, which I'll compare to my printf
 # include <stdio.h>
 
 int		ft_printf(const char *s, ...);
