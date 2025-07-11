687f292062aca282fcf7ee2be0858c578ababf35
edwo-rei <edwo-rei@student.42malaga.com>
2025-07-11 12:25:01 +0200

    Updated push_swap project

---
+++ push_swap/push_swap.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edwo-rei <edwo-rei@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/11 11:02:46 by edwo-rei          #+#    #+#             */
+/*   Updated: 2025/07/11 12:00:42 by edwo-rei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char *argv[])
+{
+	//declare variables for stacks a * b (circular linked lists?)
+	int	i;
+
+	//guard against no args
+	if (argc < 2)
+		return (0);
+	//mechanism to to deal with an unknown # of args
+	i = argc - 1;
+	while (i > 0)
+	{
+		//check if string is an int
+		//check if int is a duplicate
+		//allocate memory for each input
+		i--;
+	}
+}
---
+++ push_swap/push_swap.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edwo-rei <edwo-rei@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/10 18:16:47 by edwo-rei          #+#    #+#             */
+/*   Updated: 2025/07/10 18:34:47 by edwo-rei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* INCLUDE GUARD */
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+/* LIBRARIES */
+//stdlib contains malloc, free & exit
+# include <stdlib.h>
+//unistd library contains write & read
+# include <unistd.h>
+//stdio library contains printf, which I'll use for testing
+# include <stdio.h>
+
+/* MY HEADER FILES */
+//libft header file contains the prototypes of my libft functions
+# include "libft/libft.h"
+//ft_printf header file contains prototypes of my ft_printf functions
+# include "ft_printf/ft_printf.h"
+
+
+#endif
