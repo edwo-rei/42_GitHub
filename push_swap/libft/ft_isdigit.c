d0bc457bb8641a1974354e8b62eebd08dfd48bf3
edwo-rei <edwo-rei@student.42malaga.com>
2025-07-16 12:07:34 +0200

    Tried to fix ft_isdigit.c problem

--- push_swap/libft/ft_isdigit.c
+++ push_swap/libft/ft_isdigit.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: edwo-rei <edwo-rei@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/23 20:14:44 by edwo-rei          #+#    #+#             */
-/*   Updated: 2025/07/09 19:53:53 by edwo-rei         ###   ########.fr       */
+/*   Created: 2025/04/23 19:21:36 by edwo-rei          #+#    #+#             */
+/*   Updated: 2025/04/24 17:55:12 by edwo-rei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,12 @@ int	ft_isdigit(int c)
 		return (c);
 	return (0);
 }
+/*
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		printf("%i\n", isdigit(argv[1][0]));
+		printf("%i\n", ft_isdigit(argv[1][0]));
+	}
+}*/
